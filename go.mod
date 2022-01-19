5e60a982b50c2535271fbdc62139556f918076e2
molson82 <wvandebelt82@yahoo.com>
2022-01-19 18:34:10 -0500

    Hello World

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/site-tech/hackerEarth-iot
+
+go 1.17
+
+require github.com/quasilyte/go-ruleguard/dsl v0.3.13
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/quasilyte/go-ruleguard/dsl"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
