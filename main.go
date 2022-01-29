b232882d5dcde7f5683204a347b775f20f869190
molson82 <wvandebelt82@yahoo.com>
2022-01-29 00:02:38 -0500

    sending data

--- main.go
+++ main.go
@@ -13,18 +13,19 @@ import (
 
 func main() {
 	// url and REST method to send data to
-	url := "http://localhost:49565/api/v1/resource/RESTDevice/int16_reading"
+	url := "http://localhost:49565/api/v1/resource/RESTDevice/float32_reading"
 	method := "POST"
 	// buld REST client
 	client := &http.Client{}
 
-	for i := 0; i < 10; i++ {
-
+	for {
 		// generate random data
+		time.Sleep(time.Second)
 		rand.Seed(time.Now().UnixNano())
-		min := 1
-		max := 999
+		min := 0
+		max := 40
 		value := rand.Intn(max-min+1) + min
+		fmt.Println(value)
 		payload := strings.NewReader(fmt.Sprint(value))
 
 		req, err := http.NewRequest(method, url, payload)
@@ -45,15 +46,10 @@ func main() {
 		defer res.Body.Close()
 
 		// read in response body
-		body, err := io.ReadAll(res.Body)
+		_, err = io.ReadAll(res.Body)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-
-		// print status code
-		fmt.Println(res.StatusCode)
-		// print response body
-		fmt.Println(string(body))
 	}
 }
